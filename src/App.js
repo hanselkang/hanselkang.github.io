d2339f6682fb8bf2d897b070cc6ee7c3d1ce3cc3
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-06-17 19:37:02 +0100

    react reminding and menu setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import MainContainer from './container/MainContainer';
 
 function App() {
   return (
     <div className="App">
       <h1>Still Planning....</h1>
+      <MainContainer />
+
     </div>
   );
 }
---
+++ src/container/MainContainer.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const MainContainer = () => {
+
+    const main = <h1>Home</h1>
+    const works = <h1>Works</h1>
+    const photos = <h1>Photos</h1>
+    const otherWorks = <h1>Other Works</h1>
+    const github = <h1>Git Hub</h1>
+    const instagram = <h1>Instagram</h1>
+    const cats = <h1>Cats</h1>
+    const garden = <h1>Garden</h1>
+    const youtube = <h1>Youtube</h1>
+
+
+    const [selectedAct, setSelectedAct] = useState(main)
+
+    const handleMenu = (event) => {
+        console.log(event.target)
+        if (event.target.innerText === "Cats") {
+            setSelectedAct(cats);
+        } else if (event.target.innerText === "Works") {
+            setSelectedAct(works);
+        } else if (event.target.innerText === "Other Works") {
+            setSelectedAct(otherWorks);
+        } else if (event.target.innerText === "Photos") {
+            setSelectedAct(photos);
+        } else if (event.target.innerText === "Github") {
+            setSelectedAct(github);
+        } else if (event.target.innerText === "Instagram") {
+            setSelectedAct(instagram);
+        } else if (event.target.innerText === "Youtube") {
+            setSelectedAct(youtube);
+        } else if (event.target.innerText === "Garden") {
+            setSelectedAct(garden);
+        } else {
+            setSelectedAct(main);
+        }
+
+    }
+
+    return (
+        <>
+            <div>
+                <div onClick={handleMenu}>Home</div>
+                <div onClick={handleMenu}>Works</div>
+                <div onClick={handleMenu}>Other Works</div>
+                <div onClick={handleMenu}>Photos</div>
+                <div onClick={handleMenu}>Github</div>
+                <div onClick={handleMenu}>Instagram</div>
+                <div onClick={handleMenu}>Cats</div>
+                <div onClick={handleMenu}>Garden</div>
+                <div onClick={handleMenu}>Youtube</div>
+                
+                <div>{selectedAct}</div>
+            </div>
+        </>
+    )
+}
+
+export default MainContainer
