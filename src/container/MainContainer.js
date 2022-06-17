0d30e888fee56e667793f94d567f0361404dbcab
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-06-17 19:41:51 +0100

    menu

--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -5,12 +5,15 @@ const MainContainer = () => {
     const main = <h1>Home</h1>
     const works = <h1>Works</h1>
     const photos = <h1>Photos</h1>
-    const otherWorks = <h1>Other Works</h1>
+    const previousWorks = <h1>Previous Works</h1>
     const github = <h1>Git Hub</h1>
     const instagram = <h1>Instagram</h1>
     const cats = <h1>Cats</h1>
     const garden = <h1>Garden</h1>
     const youtube = <h1>Youtube</h1>
+    const linkedin = <h1>LinkedIn</h1>
+    const twitter = <h1>Twitter</h1>
+    
 
 
     const [selectedAct, setSelectedAct] = useState(main)
@@ -21,8 +24,8 @@ const MainContainer = () => {
             setSelectedAct(cats);
         } else if (event.target.innerText === "Works") {
             setSelectedAct(works);
-        } else if (event.target.innerText === "Other Works") {
-            setSelectedAct(otherWorks);
+        } else if (event.target.innerText === "Previous Works") {
+            setSelectedAct(previousWorks);
         } else if (event.target.innerText === "Photos") {
             setSelectedAct(photos);
         } else if (event.target.innerText === "Github") {
@@ -33,6 +36,10 @@ const MainContainer = () => {
             setSelectedAct(youtube);
         } else if (event.target.innerText === "Garden") {
             setSelectedAct(garden);
+        } else if (event.target.innerText === "LinkedIn") {
+            setSelectedAct(linkedin);
+        } else if (event.target.innerText === "Twitter") {
+            setSelectedAct(twitter);
         } else {
             setSelectedAct(main);
         }
@@ -44,13 +51,15 @@ const MainContainer = () => {
             <div>
                 <div onClick={handleMenu}>Home</div>
                 <div onClick={handleMenu}>Works</div>
-                <div onClick={handleMenu}>Other Works</div>
+                <div onClick={handleMenu}>Previous Works</div>
                 <div onClick={handleMenu}>Photos</div>
                 <div onClick={handleMenu}>Github</div>
                 <div onClick={handleMenu}>Instagram</div>
                 <div onClick={handleMenu}>Cats</div>
                 <div onClick={handleMenu}>Garden</div>
                 <div onClick={handleMenu}>Youtube</div>
+                <div onClick={handleMenu}>LinkedIn</div>
+                <div onClick={handleMenu}>Twitter</div>
                 
                 <div>{selectedAct}</div>
             </div>
