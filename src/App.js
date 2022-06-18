57b0a80d83de44a6d855480ff86a78b2c1b48a13
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-06-18 19:24:44 +0100

    menu is building

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import MainContainer from './container/MainContainer';
 function App() {
   return (
     <div className="App">
-      <h1>Still Planning....</h1>
       <MainContainer />
     </div>
   );
--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -3,14 +3,19 @@ import MainDrawing from "../components/MainDrawing"
 
 const MainContainer = () => {
 
-    const main = <h1>Home</h1>
+    const main = <h1> </h1>
     const works = <h1>Works</h1>
-    const photos = <h1>Photos</h1>
-    const previousWorks = <h1>Previous Works</h1>
+    const about =
+        <div>
+            A Software Developer<br /> with a background in <br />Architecture Design <br />and Photograph<br />
+            in Scotland.<br /><br />
+            I am into learning, <br />
+            planning <br />
+            and designing.<br />
+            
+        </div>
     const github = <h1>Git Hub</h1>
     const instagram = <h1>Instagram</h1>
-    const cats = <h1>Cats</h1>
-    const garden = <h1>Garden</h1>
     const youtube = <h1>Youtube</h1>
     const linkedin = <h1>LinkedIn</h1>
     const twitter = <h1>Twitter</h1>
@@ -22,22 +27,16 @@ const MainContainer = () => {
 
     const handleMenu = (event) => {
         console.log(event.target)
-        if (event.target.innerText === "Cats") {
-            setSelectedAct(cats);
+        if (event.target.innerText === "Github") {
+            setSelectedAct(github);
         } else if (event.target.innerText === "Works") {
             setSelectedAct(works);
-        } else if (event.target.innerText === "Previous Works") {
-            setSelectedAct(previousWorks);
-        } else if (event.target.innerText === "Photos") {
-            setSelectedAct(photos);
-        } else if (event.target.innerText === "Github") {
-            setSelectedAct(github);
+        } else if (event.target.innerText === "About") {
+            setSelectedAct(about);
         } else if (event.target.innerText === "Instagram") {
             setSelectedAct(instagram);
         } else if (event.target.innerText === "Youtube") {
             setSelectedAct(youtube);
-        } else if (event.target.innerText === "Garden") {
-            setSelectedAct(garden);
         } else if (event.target.innerText === "LinkedIn") {
             setSelectedAct(linkedin);
         } else if (event.target.innerText === "Twitter") {
@@ -51,26 +50,25 @@ const MainContainer = () => {
     }
 
     return (
-        <>
-            <div>
+        <div className="parent">
+            <div className="div0"><h1 onClick={handleMenu}>Hansel Kang</h1></div>
+            <div className="div1">
                 <div onClick={handleMenu}>Home</div>
+                <div onClick={handleMenu}>About</div>
+                <br />
                 <div onClick={handleMenu}>Works</div>
-                <div onClick={handleMenu}>Previous Works</div>
-                <div onClick={handleMenu}>Photos</div>
+                <br />
                 <div onClick={handleMenu}>Github</div>
-                <div onClick={handleMenu}>Instagram</div>
-                <div onClick={handleMenu}>Cats</div>
-                <div onClick={handleMenu}>Garden</div>
-                <div onClick={handleMenu}>Youtube</div>
                 <div onClick={handleMenu}>LinkedIn</div>
+                <div onClick={handleMenu}>Instagram</div>
+                <div onClick={handleMenu}>Youtube</div>
                 <div onClick={handleMenu}>Twitter</div>
                 <div onClick={handleMenu}>email</div>
-
-                <div>{selectedAct}</div>
             </div>
-
-            <div><MainDrawing/></div>
-        </>
+            <div className="div2"><MainDrawing /></div>
+            <div className="div3">{selectedAct}</div>
+            <div className="div4"> 2022 hanseul.com all rights reserved</div>
+        </div>
     )
 }
 
