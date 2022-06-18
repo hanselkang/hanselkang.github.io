e6ae07ad20ced91df69fa8bb5115e4e7f4d56699
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-06-18 23:07:10 +0100

    menu fixed, copyright and social button

--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -1,25 +1,21 @@
 import React, { useState } from "react";
 import MainDrawing from "../components/MainDrawing"
+import linkedin from "../components/src/icons/linkedin.png"
+import github from "../components/src/icons/github.png"
+import twitter from "../components/src/icons/twitter.png"
+import instagram from "../components/src/icons/instagram.png"
+import youtube from "../components/src/icons/youtube.png"
 
 const MainContainer = () => {
 
-    const main = <h1> </h1>
-    const works = <h1>Works</h1>
-    const about =
-        <div>
-            A Software Developer<br /> with a background in <br />Architecture Design <br />and Photograph<br />
-            I am in Scotland.<br /><br />
-            I am into learning, <br />
-            planning <br />
-            and designing.<br />
-            
-        </div>
-    const github = <h1>Git Hub</h1>
-    const instagram = <h1>Instagram</h1>
-    const youtube = <h1>Youtube</h1>
-    const linkedin = <h1>LinkedIn</h1>
-    const twitter = <h1>Twitter</h1>
-    const email = <h1>email</h1>
+
+
+
+    const programming = <h1> </h1>
+    const main = <h1></h1>
+    const photograph = <h3></h3>
+    const other = <h3> </h3>
+    const email = <h3> </h3>
 
 
 
@@ -27,20 +23,12 @@ const MainContainer = () => {
 
     const handleMenu = (event) => {
         console.log(event.target)
-        if (event.target.innerText === "Github") {
-            setSelectedAct(github);
-        } else if (event.target.innerText === "Works") {
-            setSelectedAct(works);
-        } else if (event.target.innerText === "About") {
-            setSelectedAct(about);
-        } else if (event.target.innerText === "Instagram") {
-            setSelectedAct(instagram);
-        } else if (event.target.innerText === "Youtube") {
-            setSelectedAct(youtube);
-        } else if (event.target.innerText === "LinkedIn") {
-            setSelectedAct(linkedin);
-        } else if (event.target.innerText === "Twitter") {
-            setSelectedAct(twitter);
+        if (event.target.innerText === "Photograph") {
+            setSelectedAct(photograph);
+        } else if (event.target.innerText === "Prgoramming") {
+            setSelectedAct(programming);
+        } else if (event.target.innerText === "Paint & Previous") {
+            setSelectedAct(other);
         } else if (event.target.innerText === "email") {
             setSelectedAct(email);
         } else {
@@ -53,21 +41,43 @@ const MainContainer = () => {
         <div className="parent">
             <div className="div0"><h1 onClick={handleMenu}>Hansel Kang</h1></div>
             <div className="div1">
-                <div onClick={handleMenu}>Home</div>
-                <div onClick={handleMenu}>About</div>
+                <div>Portfolio</div>
                 <br />
-                <div onClick={handleMenu}>Works</div>
+                <div onClick={handleMenu}>Programming</div>
+                <div onClick={handleMenu}>Photograph</div>
+                <div onClick={handleMenu}>Paint & Previous</div>
                 <br />
-                <div onClick={handleMenu}>Github</div>
-                <div onClick={handleMenu}>LinkedIn</div>
-                <div onClick={handleMenu}>Instagram</div>
-                <div onClick={handleMenu}>Youtube</div>
-                <div onClick={handleMenu}>Twitter</div>
                 <div onClick={handleMenu}>email</div>
+
+                <div>{selectedAct}</div>
             </div>
             <div className="div2"><MainDrawing /></div>
-            <div className="div3">{selectedAct}</div>
-            <div className="div4"> 2022 hanseul.com all rights reserved</div>
+            <div className="div3">
+                Software Developer,<br />
+                Photographer<br />
+                in Scotland.<br /><br />
+                learning, <br />
+                planning, <br />
+                designing.<br />
+            </div>
+            <div className="div4">
+                <a href="https://www.linkedin.com/in/hansel-kang/" target="_blank" >
+                    <img src={linkedin} width="30" />
+                </a>
+                <a href="https://github.com/hanselkang" target="_blank" >
+                    <img src={github} width="30" />
+                </a>
+                <a href="https://twitter.com/hihansel" target="_blank" >
+                    <img src={twitter} width="30" />
+                </a>
+                <a href="https://www.instagram.com/hansel_in_scotland/" target="_blank">
+                    <img src={instagram} width="30" />
+                </a>
+                <a href="https://youtu.be/4i7ZvnOR9NU" target="_blank" >
+                    <img src={youtube} width="30" />
+                </a>
+            </div>
+            <div className="div5">2022 hanseul.com all rights reserved</div>
         </div>
     )
 }
